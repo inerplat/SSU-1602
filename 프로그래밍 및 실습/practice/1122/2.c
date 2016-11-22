2d1c4ad6cf700de18521b8b4dacf6df015ff67bb
DH Kim <inerplat@gmail.com>
2016-11-22 10:24:08 +0900

    Update 2.c

--- 프로그래밍 및 실습/practice/1122/2.c
+++ 프로그래밍 및 실습/practice/1122/2.c
@@ -4,16 +4,16 @@
 int main()
 {
 	int s[CLASSES][STUDENTS] = { {87,77,75,97,90},{65,85,90,70,80},{80,65,95,100,75} };
-	int clas, student, total=0, subtotal=0;
+	int clas, student, total=0, subtotal;
 	for (clas = 0; clas < CLASSES; clas++)
 	{
+		subtotal = 0;
 		for (student = 0; student < STUDENTS; student++)
 		{
 			subtotal += s[clas][student];
 		}
 		printf("학급 %d의 평균 성적 = %d\n", clas + 1, subtotal / STUDENTS);
 		total += subtotal;
-		subtotal = 0;
 	}
 	printf("전체 학생들의 평균 성적 = %d\n", total/(CLASSES*STUDENTS));
 	return 0;
