2af0d030e4ef1023a4f0c4a522d59eaeec63af08
DH Kim <inerplat@gmail.com>
2016-11-29 10:15:57 +0900

    Create 2.c

---
+++ 프로그래밍 및 실습/practice/1129/2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+	int i = 10;
+	int *pi = &i;
+	printf("i=%d, pi=%p\n", i, pi);
+	(*pi)++;
+	printf("i=%d, pi=%p\n", i, pi);
+	printf("\ni=%d, pi=%p\n", i, pi);
+	*pi++;
+	printf("i=%d, pi=%p\n", i, pi);
+	return 0;
+}
