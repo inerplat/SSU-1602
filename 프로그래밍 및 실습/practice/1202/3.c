45747fe719821b638a3755eaff186c68f6abd66c
DH Kim <inerplat@gmail.com>
2016-12-02 09:57:02 +0900

    Update 3.c

--- 프로그래밍 및 실습/practice/1202/3.c
+++ 프로그래밍 및 실습/practice/1202/3.c
@@ -5,10 +5,12 @@ void swap(int *a, int *b)
 	tmp = *a;
 	*a = *b;
 	*b = tmp;
+	return;
 }
 int main()
 {
 	int a = 10, b = 20;
+	printf("%d %d\n", a, b);
 	swap(&a, &b);
 	printf("%d %d\n", a, b);
 	return 0;
