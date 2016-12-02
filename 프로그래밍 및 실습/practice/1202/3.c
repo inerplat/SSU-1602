89f142074e3ab9145d716010d18dc620e687b080
DH Kim <inerplat@gmail.com>
2016-12-02 09:54:08 +0900

    Create 3.c

---
+++ 프로그래밍 및 실습/practice/1202/3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void swap(int *a, int *b)
+{
+	int tmp;
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+int main()
+{
+	int a = 10, b = 20;
+	swap(&a, &b);
+	printf("%d %d\n", a, b);
+	return 0;
+}
